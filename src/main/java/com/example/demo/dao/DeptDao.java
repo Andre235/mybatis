0b4664d566e13d80fe415d62bad96d198cdaf026
zhaojingchao <2356430623@qq.com>
2020-10-30 07:40:28 +0800

    模糊匹配

--- src/main/java/com/example/demo/dao/DeptDao.java
+++ src/main/java/com/example/demo/dao/DeptDao.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import com.example.demo.entity.DeptEntity;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: geek
@@ -39,4 +40,16 @@ public interface DeptDao {
      * @param id 部门id
      */
     void deleteById(Integer id);
+
+    /**
+     * @param map
+     * @return 新增部门数据
+     */
+    Integer createDeptByMap(Map<String,Object> map);
+
+    /**
+     * @param map 部门名称
+     * @return 通过部门名称模糊查询
+     */
+    List<DeptEntity> getListByDeptNameLike(Map<String,Object> map);
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,17 +4,30 @@ import com.example.demo.dao.DeptDao;
 import com.example.demo.entity.DeptEntity;
 import com.example.demo.util.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
+    private SqlSession sqlSession = null;
+    private DeptDao mapper = null;
+
+    @BeforeEach
+    public void before(){
+        sqlSession = MybatisUtils.getSqlSession();
+        mapper = sqlSession.getMapper(DeptDao.class);
+    }
+
+    @AfterEach
+    public void after(){
+        sqlSession.close();
     }
 
     @Test
@@ -62,5 +75,23 @@ class DemoApplicationTests {
         sqlSession.close();
     }
 
+    @Test
+    public void createDeptByMapTest(){
+        Map<String, Object> map = new HashMap<>(2);
+        map.put("key",10);
+        map.put("deptName","项目主管部门");
+        Integer result = mapper.createDeptByMap(map);
+        sqlSession.commit();
+        if(result > 0){
+            System.out.println("插入数据成功");
+        }
+    }
 
+    @Test
+    public void getListByDeptNameLike(){
+        Map<String, Object> map = new HashMap<>(2);
+        map.put("name","'%端%' or 1 = 1");
+        List<DeptEntity> list = mapper.getListByDeptNameLike(map);
+        list.forEach(System.out::println);
+    }
 }
